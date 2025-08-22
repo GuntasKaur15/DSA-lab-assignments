5ecfc3975fd0a7991664d4b54b042ed018ba7d68
GuntasKaur15 <gkaur5_be24@thapar.edu>
2025-08-22 09:36:10 +0530

    done

---
+++ 1st assignment/q1ass1.cpp
@@ -0,0 +1,124 @@
+// 1) Develop a Menu driven program to demonstrate the following operations of Arrays 
+// ——MENU——- 
+// 1.CREATE 
+// 2. DISPLAY 
+// 3. INSERT 
+// 4. DELETE 
+// 5. LINEAR SEARCH 
+// 6. EXIT
+
+
+#include<iostream>
+using namespace std;
+
+void create(int arr[], int &n){
+    for (int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+}
+void display(int arr[], int &n){
+    for (int i=0; i<n; i++){
+        cout<<arr[i];
+    }
+    cout<<endl;
+}
+void insert_an_element(int arr[], int &n){
+    int k;
+    cout<<"enter the position where element has to be inserted";
+    cin>>k;
+    int a;
+    cout<<"enter the number u want to insert"<<endl;
+    cin>>a;
+    for (int i=n ; i>=k; i--){
+        arr[i]=arr[i-1];
+    }
+    arr[k-1]=a;
+    n++;
+    for (int i=0; i<n; i++){
+        cout<<arr[i];
+    }
+    cout<<endl;
+}
+void delete_an_element(int arr[], int &n){
+    int k;
+    cout<<"enter the position where element has to be deleted";
+    cin>>k;
+   
+        for (int i=k; i<n; i++){
+            arr[i]=arr[i+1];
+        }
+        n--;
+        for (int i=0; i<n; i++){
+            cout<<arr[i];
+        }
+        cout<<endl;
+}
+void linear_search(int arr[], int &n){
+    int a;
+    cout<<"enter the element u want to be searched";
+    cin>>a;
+    bool found=false;
+    for (int i=0 ; i<n; i++){
+        if (arr[i]==a){
+            cout<<"the element is found at " <<i+1<< " th position";
+            found =true;
+        }
+    }
+    if (found== false){
+        cout<<"the element is not present ";
+    }
+}
+
+int main(){
+    int arr[100];
+
+    int n; 
+    cout<<"enter the size-";
+    cin>>n;
+
+    
+    int choice;
+    do
+    {
+        cout << "1. CREATE" <<endl;
+        cout << "2. DISPLAY" <<endl;
+        cout << "3. INSERT" <<endl;
+        cout << "4. DELETE" <<endl;
+        cout << "5. LINEAR SEARCH" <<endl;
+        cout << "6. EXIT" <<endl;
+        cout << "Enter your choice: ";
+        cin>>choice;
+        
+        switch (choice)
+    {
+        
+    case 1:
+    create(arr,n);
+    break;
+
+    case 2:
+    display(arr,n);
+    break;
+    
+    case 3:
+    insert_an_element(arr,n);
+    break;
+    
+    case 4:
+    delete_an_element(arr,n);
+    break;
+    
+    case 5:
+    linear_search(arr,n);
+    break;
+    
+    case 6:
+    cout<<"exiting the program";
+    break;
+    
+    default:
+    cout<<"invalid";
+}
+} while (choice!=6);
+return 0;
+}
